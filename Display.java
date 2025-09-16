3f4cde56fac2eef50b5bb4a6efec746bad5f7c54
arthur-siqueira <arthurhsiqueira02@gmail.com>
2025-09-16 15:09:48 -0300

    Adiciona as novas funcionalidades das classes

--- Display.java
+++ Display.java
@@ -1,6 +1,42 @@
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+
 public class Display {
     
     public void gerarImagem(Hidrometro h) {
-        
+        try {
+            int largura = 400, altura = 200;
+            BufferedImage img = new BufferedImage(largura, altura, BufferedImage.TYPE_INT_RGB);
+            Graphics2D g = img.createGraphics();
+
+            // Fundo branco
+            g.setColor(Color.WHITE);
+            g.fillRect(0, 0, largura, altura);
+
+            // Número de série
+            g.setColor(Color.BLACK);
+            g.setFont(new Font("SansSerif", Font.BOLD,18));
+            g.drawString("Hidrômetro Nº " + h.getNumeroSerie(), 50, 40);
+
+            // Consumo total em litros
+            g.setFont(new Font("Monospaced", Font.BOLD, 40));
+            g.drawString(String.format("%06d", h.getLitrosConsumidos()), 50, 90);
+
+            // Conversão em m³
+            g.setFont(new Font("SansSerif", Font.PLAIN, 22));
+            g.drawString("Litros restantes: " + h.getLitrosRestantes(), 50, 160);
+            g.drawString("Décimo de litro: " + h.getDecimosLitro(), 50, 185);
+
+            g.dispose();
+
+            // Salvar imagem
+            ImageIO.write(img, "png", new File("hidrometro_saida.png"));
+            System.out.println("Imagem gerada: hidrometro_saida.png");
+            
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
--- Simulador.java
+++ Simulador.java
@@ -1,3 +1,15 @@
 public class Simulador {
+    public static void main(String[] args) {
+        Hidrometro h = new Hidrometro("12345-ABC");
+        Display d = new Display();
+
+        // Simulação de consumo
+        h.consumirAgua(50);
+        h.consumirAgua(12);
+        h.consumirAgua(120);
+
+        // Gera a imagem com display
+        d.gerarImagem(h);
+    }
     
 }
